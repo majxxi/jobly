58c522be981e50880d586717cc3506998557cc33
majick <majicktadepa@gmail.com>
2020-08-24 13:31:20 -0700

    modify api base_url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.BASE_URL || "http://jobly-demo-mt.herokuapp.com";
 
 class JoblyApi {
   static async request(endpoint, params = {}, verb = "get") {
